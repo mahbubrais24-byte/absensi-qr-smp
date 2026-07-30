9047df6e4660df2256532bd454c6de7280f57b95
mahbubrais24-byte <mahbub.rais24@guru.smp.belajar.id>
2026-07-30 16:58:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,124 @@
+// ===========================
+// KONFIGURASI
+// ===========================
 
+// GANTI DENGAN URL WEB APP APPS SCRIPT BAPAK
+const API_URL = "https://script.google.com/macros/s/AKfycbwJYCfcKvqOUsSspYHJlJ3sqNzMYx6XkoY5C0ZLcX1Sbkce1F5v4uFYkr5L--6rqDWT/exec";
+
+let html5QrCode = null;
+let modeAbsen = "";
+let sedangScan = false;
+
+// ===========================
+// PILIH MODE
+// ===========================
+
+function pilihMode(mode){
+
+    modeAbsen = mode;
+
+    document.getElementById("hasil").innerHTML =
+        "<b>Mode : " + mode + "</b><br>Mengaktifkan kamera...";
+
+    mulaiScanner();
+
+}
+
+// ===========================
+// MULAI SCANNER
+// ===========================
+
+async function mulaiScanner(){
+
+    if(html5QrCode){
+
+        try{
+            await html5QrCode.stop();
+        }catch(e){}
+
+        try{
+            await html5QrCode.clear();
+        }catch(e){}
+    }
+
+    html5QrCode = new Html5Qrcode("reader");
+
+    html5QrCode.start(
+
+        {
+            facingMode:"environment"
+        },
+
+        {
+            fps:10,
+            qrbox:250
+        },
+
+        suksesScan,
+
+        function(){}
+
+    );
+
+}
+
+// ===========================
+// SAAT QR BERHASIL
+// ===========================
+
+async function suksesScan(decodedText){
+
+    if(sedangScan) return;
+
+    sedangScan = true;
+
+    document.getElementById("hasil").innerHTML =
+        "Mengirim data...";
+
+    try{
+
+        const response = await fetch(API_URL,{
+
+            method:"POST",
+
+            headers:{
+                "Content-Type":"application/json"
+            },
+
+            body:JSON.stringify({
+
+                nis:decodedText.trim(),
+                mode:modeAbsen
+
+            })
+
+        });
+
+        const hasil = await response.json();
+
+        if(hasil.status){
+
+            document.getElementById("hasil").innerHTML =
+                "✅ " + hasil.pesan;
+
+        }else{
+
+            document.getElementById("hasil").innerHTML =
+                "❌ " + hasil.pesan;
+
+        }
+
+    }catch(err){
+
+        document.getElementById("hasil").innerHTML =
+            "❌ " + err;
+
+    }
+
+    setTimeout(function(){
+
+        sedangScan = false;
+
+    },3000);
+
+}
