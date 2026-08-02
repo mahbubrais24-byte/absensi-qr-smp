ed74c3d72a005668f2c926f2767928a5e271ad00
mahbubrais24-byte <mahbub.rais24@guru.smp.belajar.id>
2026-08-02 17:36:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = https://script.google.com/macros/s/AKfycbzw6CoOg9xNm6-dGgJxeWZDGKoOne6wIHUU4p6wZhNy3Si9KY9KrrEUBvh1kSvmi368/exec
+const API_URL = "https://script.google.com/macros/s/AKfycbzw6CoOg9xNm6-dGgJxeWZDGKoOne6wIHUU4p6wZhNy3Si9KY9KrrEUBvh1kSvmi368/exec"
 
 const scanner = new Html5Qrcode("reader");
 
@@ -15,7 +15,7 @@ scanner.start(
 
     document.getElementById("nis").innerHTML = decodedText;
 
-}
+},
 
     function(error){
 
