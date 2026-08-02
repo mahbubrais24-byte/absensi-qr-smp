4d93d9a15109c01b5c2078a4cea3afd2327f9da0
mahbubrais24-byte <mahbub.rais24@guru.smp.belajar.id>
2026-08-02 16:47:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const scanner = new Html5Qrcode("reader");
 
+scanner.start(
+
+    { facingMode: "environment" },
+
+    {
+        fps: 10,
+        qrbox: 250
+    },
+
+    function(decodedText){
+
+        alert("QR : " + decodedText);
+
+    },
+
+    function(error){
+
+        // abaikan
+
+    }
+
+);
