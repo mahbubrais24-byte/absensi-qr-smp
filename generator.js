ffb9cdb57b331ad22db2eb2dd7e127c63e137755
mahbubrais24-byte <mahbub.rais24@guru.smp.belajar.id>
2026-08-04 10:45:10 +0700

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,1 +1,43 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbzw6CoOg9xNm6-dGgJxeWZDGKoOne6wIHUU4p6wZhNy3Si9KY9KrrEUBvh1kSvmi368/exec";
 
+document.getElementById("btnCari").onclick = async function(){
+
+    const nis = document.getElementById("nisCari").value.trim();
+
+    if(nis==""){
+
+        alert("Masukkan NIS");
+
+        return;
+
+    }
+
+    const response = await fetch(
+
+        API_URL +
+
+        "?api=carisiswa&nis="+encodeURIComponent(nis)
+
+    );
+
+    const hasil = await response.json();
+
+    if(!hasil.status){
+
+        document.getElementById("hasil").innerHTML=
+
+        "<h2>NIS tidak ditemukan</h2>";
+
+        return;
+
+    }
+
+    document.getElementById("hasil").innerHTML=
+
+    "<h2>"+hasil.siswa.nama+"</h2>"+
+
+    "<p>NIS : "+hasil.siswa.nis+"</p>"+
+
+    "<p>Kelas : "+hasil.siswa.kelas+"</p>";
+
+};
