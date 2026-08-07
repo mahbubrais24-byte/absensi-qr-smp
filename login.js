f6d4f94f97c103fe46d1c4604d42d2a6c5ca26a9
mahbubrais24-byte <mahbub.rais24@guru.smp.belajar.id>
2026-08-07 12:53:20 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,19 @@
+document.getElementById("btnLogin").addEventListener("click", function(){
 
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const pesan = document.getElementById("pesan");
+
+    if(username === "" || password === ""){
+        pesan.innerHTML = "❌ Username dan Password wajib diisi.";
+        return;
+    }
+
+    pesan.style.color = "green";
+    pesan.innerHTML = "✅ Login berhasil...";
+
+    setTimeout(function(){
+        window.location.href = "admin.html";
+    },1000);
+
+});
